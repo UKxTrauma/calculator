44575f97503de784ecdffa47ed1b3eff2d7ad5f0
Brett Garrett <b.t.garrett@aol.co.uk>
2022-08-10 17:48:08 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { evaluate } from "mathjs";
+
 import './App.css';
+import Input from "./components/Input";
 
 function App() {
+  const [result, setResult] = useState("0");
+  const [text, setText] = useState("");
+
+  const toText = (val) => {
+    setText(text + val);
+  };
+
+  const toResult = (val) => {
+    setResult(val);
+  };
+
+  const clickText = (inside) => {
+    toText(inside);
+  };
+
+  const clickResult = () => {
+    toResult(evaluate(text))
+  };
+
+  const clearInput = () => {
+    setResult("0");
+    setText("");
+  };
+
+  const buttons = ['(',')','%','AC',7,8,9,'/',4,5,6,'*',1,2,3,'-',0,'.','=','+'];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='screen'>
+        <Input text={text} result={result}/>
+      </div>
+      <div className="buttonBox">
+        {buttons.map((button, index) => {
+          if(index === 3){
+            return <Button key={index} label={button} click={() => clearInput()} />;
+          } else if(index === 18) {
+            return <Button key={index} label={button} click={() => clickResult()} />;
+          } else {
+            return <Button key={index} label={button} click={() => clickText(button)} />;
+          }
+        })}
+      </div>
     </div>
   );
-}
+};
 
+const Button = (props) => {
+  return <button className="button" onClick={props.click}>{props.label}</button>;
+};
 export default App;
